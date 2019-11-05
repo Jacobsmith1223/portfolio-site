013abfc1c6245b3ac7f282fddc961dd6a451e36b
Jake Smith <jacob_smith1223@me.com>
2019-11-05 15:21:50 -0700

    portfolio done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import pic01 from './profile-pic.jpg'
+import pic02 from './gamebuddies.png'
+import pic03 from './fakeestate.png'
 
 class Main extends React.Component {
   render() {
@@ -21,39 +21,6 @@ class Main extends React.Component {
         id="main"
         style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
       >
-        <article
-          id="intro"
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Intro</h2>
-          <span className="image main">
-            <img src={pic01} alt="" />
-          </span>
-          <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
-          </p>
-          {close}
-        </article>
 
         <article
           id="work"
@@ -67,19 +34,24 @@ class Main extends React.Component {
             <img src={pic02} alt="" />
           </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+          React • Node.js • PostgreSQL • Express • Sockets • HTML5 • Axios • Bcrypt
           </p>
+          <h3>September 2019 - Game Buddies</h3>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            A personally built project aimed to connect gamers. Users can create a profile and join specific forums to begin chatting about the different games. 
+          </p>
+          <p>Live Site: gamebuddieslfg.com</p>
+          <p>GitHub Repo: https://github.com/Jacobsmith1223/Game-Buddies-LFG </p>
+
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+          React • Node.js • PostgreSQL • Express • Mapbox • HTML5 • Axios • Bcrypt
+          </p>
+          <h3>October 2019 - Real Estate of Utah</h3>
+          <p>
+            A group built project created to assist both realtors and homebuyers in their daily efforts to find and sell homes. Realtors can log in and add and delete homes. Homebuyers can browse the homes and find the best option for them 
           </p>
           {close}
         </article>
@@ -93,16 +65,16 @@ class Main extends React.Component {
         >
           <h2 className="major">About</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={pic01} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+          After working as a sales professional in the SaaS space for several years, I decided to change career paths and become a web developer. Working as a sales professional in both start-ups and large corporations, taught me how to collaborate across teams, communicate effectively, and meet a deadline month over month.
+          </p>
+          <p>
+          Some of my strengths include, attacking problems efficiently and with a high attention to detail. I enjoy learning new things and live for the challenge of pushing myself to become an even better web developer.  
+          </p>
+          <p>
+          When I am not developing, my passions include cars, the Utah Jazz, and spending time with my wife Rebecca and our two children
           </p>
           {close}
         </article>
@@ -147,18 +119,18 @@ class Main extends React.Component {
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
+              <a href="https://codebushi.com" className="icon fa-envelope">
+                <span className="label">Email</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
+              <a href="https://www.linkedin.com/in/jacob-smith1223/" className="icon fa-linkedin">
+                <span className="label">LinkedIn</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/Jacobsmith1223"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
